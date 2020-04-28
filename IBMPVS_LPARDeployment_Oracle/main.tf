d1817957b15fa89ea86dbf2e7c8d5252f7b6054c
FredD07 <62656495+FredD07@users.noreply.github.com>
2020-04-28 22:57:48 +0200

    Update main.tf

--- IBMPVS_LPARDeployment_Oracle/main.tf
+++ IBMPVS_LPARDeployment_Oracle/main.tf
@@ -1,10 +1,52 @@
-## Template to be used by the IBM Provider for Power Systems
 
-
-resource "local_file" "vm_private_key" {
-    content_base64    = "${var.vm_private_key_base64}"
-    filename          = "tmp/id_rsa"
+resource "ibm_pi_volume" "asm_data_volume"{
+  count = "${var.asm_data_dg_disk}"
+  pi_volume_size       = "${var.asm_data_dg_size}"
+  pi_volume_name       = "${var.vm_name}_${format("asm_data-%02d", count.index + 1)}"
+  pi_volume_type       = "${var.asm_data_type}"
+  pi_volume_shareable  = false
+  pi_cloud_instance_id = "${var.power_instance_id}"
 }
+
+data "ibm_pi_volume" "ds_asm_data_volume" {
+  depends_on           = ["ibm_pi_volume.asm_data_volume"]
+count = "${var.asm_data_dg_disk}"
+  pi_cloud_instance_id = "${var.power_instance_id}" 
+  pi_volume_name      = "${var.vm_name}_${format("asm_data-%02d", count.index + 1)}" 
+}
+
+resource "ibm_pi_volume" "asm_reco_volume"{
+  count = "${var.asm_reco_dg_disk}"
+  pi_volume_size       = "${var.asm_reco_dg_size}"
+  pi_volume_name       = "${var.vm_name}_${format("asm_reco-%02d", count.index + 1)}"
+  pi_volume_type       = "${var.asm_reco_type}"
+  pi_volume_shareable  = false
+  pi_cloud_instance_id = "${var.power_instance_id}"
+}
+
+data "ibm_pi_volume" "ds_asm_reco_volume" {
+  depends_on           = ["ibm_pi_volume.asm_reco_volume"]
+count = "${var.asm_reco_dg_disk}"
+  pi_cloud_instance_id = "${var.power_instance_id}"
+  pi_volume_name      = "${var.vm_name}_${format("asm_reco-%02d", count.index + 1)}"
+}
+
+resource "ibm_pi_volume" "asm_repo_volume"{
+  count = "2"
+  pi_volume_size       = "10"
+  pi_volume_name       = "${var.vm_name}_${format("asm_repo-%02d", count.index + 1)}"
+  pi_volume_type       = "tier1"
+  pi_volume_shareable  = false
+  pi_cloud_instance_id = "${var.power_instance_id}"
+}
+
+data "ibm_pi_volume" "ds_asm_repo_volume" {
+  depends_on           = ["ibm_pi_volume.asm_repo_volume"]
+  count = "2"
+  pi_cloud_instance_id = "${var.power_instance_id}"
+  pi_volume_name      = "${var.vm_name}_${format("asm_repo-%02d", count.index + 1)}"
+}
+
 
 data "ibm_pi_network" "power_networks" {
     count                = "${length(var.networks)}"
@@ -15,8 +57,6 @@ data "ibm_pi_network" "power_networks" {
 data "ibm_pi_image" "power_images" {
     pi_image_name        = "${var.image_name}"
     pi_cloud_instance_id = "${var.power_instance_id}"
-    #pi_image_name        = "myimage"
-    #pi_cloud_instance_id = "c7426567-d768-47ca-9741-7b4f4dbce4ae"
 }
 
 resource "ibm_pi_instance" "pvminstance" {
@@ -26,7 +66,6 @@ resource "ibm_pi_instance" "pvminstance" {
     pi_proc_type          = "${var.proc_type}"
 #    pi_migratable         = "${var.migratable}"
     pi_image_id           = "${data.ibm_pi_image.power_images.id}"
-    pi_volume_ids         = []
     pi_network_ids        = ["${data.ibm_pi_network.power_networks.*.id}"]
     pi_key_pair_name      = "${var.ssh_key_name}"
     pi_sys_type           = "${var.system_type}"
@@ -34,18 +73,21 @@ resource "ibm_pi_instance" "pvminstance" {
     pi_replication_scheme = "${var.replication_scheme}"
     pi_replicants         = "${var.replicants}"
     pi_cloud_instance_id  = "${var.power_instance_id}"
+    pi_volume_ids         = ["${ibm_pi_volume.asm_data_volume.*.volume_id}","${ibm_pi_volume.asm_reco_volume.*.volume_id}","${ibm_pi_volume.asm_repo_volume.*.volume_id}"]
 
-    provisioner "remote-exec" {
+
+provisioner "remote-exec" {
        scripts = [
             "scripts/wait_for_vm.sh",
         ]
 
-        connection {
+       connection {
             type        = "ssh"
-            host        = "${lookup(ibm_pi_instance.pvminstance.addresses[0], "externalip")}"
+            host        = "${lookup(ibm_pi_instance.pvminstance.addresses[0], "external_ip")}"
             timeout     = "15m"
             user        = "root"
             private_key = "${file("${local_file.vm_private_key.filename}")}"
         }
     }
 }
+
