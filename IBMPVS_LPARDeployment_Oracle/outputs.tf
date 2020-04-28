984607cd36ec8eaa77df56f1c035fa0885487304
FredD07 <62656495+FredD07@users.noreply.github.com>
2020-04-28 09:30:47 +0200

    Update outputs.tf

--- IBMPVS_LPARDeployment_Oracle/outputs.tf
+++ IBMPVS_LPARDeployment_Oracle/outputs.tf
@@ -1,6 +1,3 @@
-output "adress" {
-    value = "${ibm_pi_instance.pvminstance.addresses[0]}"
-}
 
 output "vm_instance_id" {
     value = "${ibm_pi_instance.pvminstance.instance_id}"
@@ -11,7 +8,7 @@ output "health_status" {
 }
 
 output "ip_address" {
-    value = "${ibm_pi_instance.pvminstance.addresses}"
+    value = "${ibm_pi_instance.pvminstance.addresses[0]}"
 }
 
 #output "progress" {
